751650299d29d1532f2bfd22aade2922d5e69ddd
J-Lakin <jacoblakin3@gmail.com>
2025-06-01 09:05:03 -0700

    Add files via upload

---
+++ optic-gaming-website/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+window.onload = init;
+
+function init() {
+   document.forms[0].onsubmit = function () {
+      if (this.checkValidity()) {
+         alert("Data passes initial validation tests");
+      }
+      return false;
+   };
+}
